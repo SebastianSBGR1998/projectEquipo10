9a87d058dd191716465258d5cc959b58a2f7324f
unknown <sebastianamezquitaleon@gmail.com>
2021-09-29 21:49:34 +0100

    subeindo React

---
+++ react/src/components/Nav/nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Fragment } from "react";
+import './Nav.css'
+
+function Nav (){
+    return ( 
+        < Fragment>
+            Nueva app
+            <ul>
+                <li> tarea1</li>
+                <li> tarea2</li>
+            </ul>
+        </Fragment>
+    )
+} 
+
+export default Nav;
