be8bdfe93a5f9f1dbfdd103f49f1aacaeb594a87
unknown <sebastianamezquitaleon@gmail.com>
2021-10-02 19:38:20 +0100

    checkout

---
+++ react/src/components/Login/login.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Fragment } from "react";
+import './Login.css';
+
+function Login (){
+    return ( 
+        < Fragment>
+            <div class="grid-container">    
+            
+                <div class="grid-item" id="cont2">
+                    <p>Iniciar Sesión</p>
+                    <br></br>
+                    <form>
+                    <input type="text" placeholder="Usuario o Correo electronico" name="user"></input>
+                    <input type="password" placeholder="Contraseña" name="password"></input>
+                    <input type="submit" class="button" value="Ingresar"></input>
+                    </form>
+                </div>
+           </div>
+        </Fragment>
+    )
+} 
+
+
+export default Login;
